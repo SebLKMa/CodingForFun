f5f48f18ff47584e967774750de3cec23cea08bf
seblkma <sebmalikkeung@gmail.com>
2016-11-24 15:34:08 +0800

    simple C++ class

---
+++ helloCpp/Greet.cpp
@@ -0,0 +1,15 @@
+/*
+ * Greet.cpp
+ *
+ *  Created on: Nov 24, 2016
+ *      Author: LMA
+ */
+#include <iostream>
+#include "Greet.h"
+using namespace std;
+
+void Greet::sayHello()
+{
+	cout << "Hello CPP!" << endl;
+}
+
--- helloCpp/hello.cpp
+++ helloCpp/hello.cpp
@@ -5,11 +5,12 @@
  *      Author: LMA
  */
 
-#include <iostream>
-using namespace std;
+#include "Greet.h"
 
 int main() {
-	cout << "Hello CPP!" << endl;
+	//cout << "Hello CPP!" << endl;
+	Greet greeter;
+	greeter.sayHello();
 	return 0;
 }
 
