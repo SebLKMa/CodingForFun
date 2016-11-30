f4b631e9f7c3c325c3a83df05408bb32148530ae
seblkma <sebmalikkeung@gmail.com>
2016-11-30 18:46:14 +0800

    C++ 11 settings and unique_ptr

--- helloCpp/hello.cpp
+++ helloCpp/hello.cpp
@@ -5,6 +5,7 @@
  *      Author: LMA
  */
 #include <iostream>
+#include <memory>
 #include "Greet.h"
 #include "HelloContainers.h"
 #include "inheritance\DerivedC.h"
@@ -23,12 +24,36 @@ void testInheritanceInHeap()
 
 	Base* pBase = new DerivedC();
 	cout << "pBase created" << endl;
-	if (0 != pBase)
+	if (nullptr != pBase)
 	{
 		//pBase->sayHello();
 		delete pBase;
 		cout << "pBase destroyed" << endl;
 	}
+}
+
+void testCpp11()
+{
+	cout << "testCpp11" << endl;
+
+	//auto text = unique_ptr<char[]>(new char[42]);
+
+    unique_ptr<DerivedC> p1(new DerivedC);  // p1 owns DerivedC
+    /*
+    if (p1) p1->bar();
+    {
+        std::unique_ptr<Foo> p2(std::move(p1));  // now p2 owns Foo
+        f(*p2);
+
+        p1 = std::move(p2);  // ownership returns to p1
+        std::cout << "destroying p2...\n";
+    }
+
+    if (p1) p1->bar();
+    */
+
+    // Foo instance is destroyed when p1 goes out of scope
+
 }
 
 int main() {
@@ -41,6 +66,7 @@ int main() {
 
 	testInheritanceInStack();
 	testInheritanceInHeap();
+	testCpp11();
 
 	return 0;
 }
