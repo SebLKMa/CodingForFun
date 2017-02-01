14f7758db7eb07b093679b6a2346ddcdca9640dd
seblkma <sebmalikkeung@gmail.com>
2017-02-01 18:22:58 +0800

    added shared dll

---
+++ cppSockets/Hello.cpp
@@ -0,0 +1,16 @@
+/*
+ * Hello.cpp
+ *
+ *  Created on: Feb 1, 2017
+ *      Author: LMA
+ */
+#include "Hello.h"
+
+using namespace std;
+
+string Hello::getHello() const
+{
+	return "Guten Tag!";
+}
+
+
---
+++ cppSockets/Hello.h
@@ -0,0 +1,19 @@
+/*
+ * Hello.h
+ *
+ *  Created on: Feb 1, 2017
+ *      Author: LMA
+ */
+
+#ifndef HELLO_H_
+#define HELLO_H_
+#include <string>
+
+class Hello
+{
+public:
+	std::string getHello() const;
+};
+
+
+#endif /* HELLO_H_ */
