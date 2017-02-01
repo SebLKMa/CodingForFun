5f40f3198ac0f62df5c1890bc75b7c0a53791d45
seblkma <sebmalikkeung@gmail.com>
2017-02-01 16:59:13 +0800

    added empty project

---
+++ cppEchoServer/src/cppEchoServer.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : cppEchoServer.cpp
+// Author      : seblkma
+// Version     :
+// Copyright   : copyright(c) seblkma
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
