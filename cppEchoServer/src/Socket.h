e5dd32d47cedc09df3b66768033da0617a4c4a24
likke <likke@LAPTOP-S2471R51>
2017-02-05 17:12:17 +0800

    Added Socket and WinsockWrapper

---
+++ cppEchoServer/src/Socket.h
@@ -0,0 +1,42 @@
+/*
+ * Socket.h
+ *
+ *  Created on: 5 Feb 2017
+ *      Author: likke
+ */
+
+#ifndef SOCKET_H_
+#define SOCKET_H_
+#include <string>
+#include <winsock2.h> // The dll libws2_32.a is found in (remove the prefix "lib" and ".o" in Libraries setting
+#include <WS2tcpip.h> // C:\msys64\mingw64\x86_64-w64-mingw32\lib
+
+class Socket
+{
+public:
+	Socket(std::string& port);
+	Socket(std::string& webAddress, std::string& port);
+	Socket(std::string& webAddress, std::string& port, addrinfo& hints);
+	~Socket();
+
+	bool IsValid();
+	int Connect();
+	int Bind();
+	int Listen(int queueSize);
+	Socket Accept();
+	SSIZE_T Send(std::stringstream data);
+	std::stringstream Receive();
+	void Close();
+
+private:
+	Socket(SOCKET newSocket, sockaddr_storage&& socketStorage);
+	void CreateSocket(std::string& webAddress, std::string port, addrinfo& hints);
+
+	addrinfo* m_pServerInfo{nullptr};
+	SOCKET m_Socket{ static_cast<SOCKET>(0xFFFFFFFF) };
+	sockaddr_storage m_AcceptedSocketStorage{};
+	socklen_t m_AcceptedSocketSize{ sizeof(m_AcceptedSocketStorage) };
+};
+
+
+#endif /* SOCKET_H_ */
--- cppEchoServer/src/cppEchoServer.cpp
+++ cppEchoServer/src/cppEchoServer.cpp
@@ -8,13 +8,53 @@
 
 #include <iostream>
 #include <string>
-#include <winsock2.h> // The dll libws2_32.a is found in (remove the prefix "lib" and ".o" in Libraries setting
-#include <WS2tcpip.h> // C:\msys64\mingw64\x86_64-w64-mingw32\lib
+#include <sstream>
+//#include <type_traits>
 #include <Hello.h>
+#include "WinsockWrapper.h"
+#include "Socket.h"
+//#include <winsock2.h> // The dll libws2_32.a is found in (remove the prefix "lib" and ".o" in Libraries setting
+//#include <WS2tcpip.h> // C:\msys64\mingw64\x86_64-w64-mingw32\lib
 
 using namespace std;
 
+void StartServer()
+{
+	WinsockWrapper myWinsockHelper;
 
+	string address{"localhost"};
+	string port{"3000"};
+	Socket bindngSocket(address, port);
+	bindngSocket.Bind();
+
+	int listenResult{ bindngSocket.Listen(5) };
+	if (listenResult == -1)
+	{
+		return;
+	}
+
+	Socket acceptingSocket{ bindngSocket.Accept() };
+	if (!acceptingSocket.IsValid())
+	{
+		return;
+	}
+
+	stringstream dataReceived{ acceptingSocket.Receive() };
+	string messageReceived;
+	getline(dataReceived, messageReceived, '\0');
+
+	cout << "Received message: " << messageReceived << endl;
+}
+
+void sayHello()
+{
+    Hello myHello;
+    const string me{"Seb"};
+    string greetings = myHello.getHello(me);
+	cout << greetings << endl;
+}
+
+/*
 void firstConnection()
 {
 	WSADATA wsaData;
@@ -46,22 +86,21 @@ void firstConnection()
 	freeaddrinfo(pServerInfo);
 	WSACleanup();
 }
+*/
 
+/**
+	NOTE:
+	remove the prefix "lib" if your DLL name starts with lib...
+	the location of DLL has to be in your runtime search PATH
+	See:
+	http://eclipsesource.com/blogs/2010/03/03/shared-libraries-with-eclipse-cdt-and-cygwin-on-windows/
+	https://tayefeh.wordpress.com/2009/07/06/creating-and-using-a-c-shared-library-with-eclipse-cdt-galileo-and-gnu-c-compiler-and-linker/
+ */
 int main(int argc, char* argv[])
 {
-	//NOTE: remove the prefix "lib" if your DLL name starts with lib...
-	//NOTE: the location of DLL has to be in your runtime search PATH
-	//See:
-	//http://eclipsesource.com/blogs/2010/03/03/shared-libraries-with-eclipse-cdt-and-cygwin-on-windows/
-	//https://tayefeh.wordpress.com/2009/07/06/creating-and-using-a-c-shared-library-with-eclipse-cdt-galileo-and-gnu-c-compiler-and-linker/
-
-    //Hello myHello;
-    //const string me{"Seb"};
-    //string greetings = myHello.getHello(me);
-
-	//cout << greetings << endl;
-
-	firstConnection();
+	sayHello();
+	//firstConnection();
+	StartServer();
 
 	return 0;
 }
