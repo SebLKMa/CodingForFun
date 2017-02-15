8972a5ef825d0bcbbc40857f1886f79c88d36c04
likke <likke@LAPTOP-S2471R51>
2017-02-15 23:53:06 +0800

    client side added code documentation

--- cppEchoClient/src/cppEchoClient.cpp
+++ cppEchoClient/src/cppEchoClient.cpp
@@ -14,6 +14,7 @@
 #include <functional> // for reference_wrapper
 #include "WinsockHelper.h"
 #include "Socket.h"
+#include "Common.h"
 //#include <winsock2.h> // The dll libws2_32.a is found in (remove the prefix "lib" and ".o" in Libraries setting
 //#include <WS2tcpip.h> // C:\msys64\mingw64\x86_64-w64-mingw32\lib
 
@@ -25,7 +26,7 @@ void SetRunning(const bool& run)
 	keepRunning = run;
 }
 
-bool SendString(std::reference_wrapper<Socket> socketRef, const string& message)
+bool SendMessage(std::reference_wrapper<Socket> socketRef, const string& message)
 {
 	stringstream requestStream{ message } ;
 	SSIZE_T result = socketRef.get().Send(move(requestStream)); // send request
@@ -33,54 +34,56 @@ bool SendString(std::reference_wrapper<Socket> socketRef, const string& message)
 	{
 		return false;
 	}
+	cout << "Message sent:" << message << endl;
 	return true;
 }
 
-void ReceiveString(std::reference_wrapper<Socket> socketRef)
+void ReceiveMessage(std::reference_wrapper<Socket> socketRef)
 {
-	cout << "ReceiveString starting" << endl;
-	//socketRef.get().SetBlockingMode(1);
-	//for (int i=0; i<2; i++)
+	cout << "ReceiveMessage starting" << endl;
+
 	while (true)
 	{
 		stringstream responseStream{ socketRef.get().Receive() }; // wait for respond
 
-		// TODO: detect disconnection
-
 		if (responseStream.rdbuf()->in_avail() == 0)
 		{
+			Common::ErrorMessage("Receive detected null stream. ReceiveMessage exiting...");
 			break;
 		}
 
 		if (!socketRef.get().IsValid()) // socket could be closed by peer
 		{
+			Common::ErrorMessage("Receive socket invalid. ReceiveMessage exiting...");
 			break;
 		}
 
 		string result;
-		getline(responseStream, result, '\0');
-		cout << result << endl;
+		getline(responseStream, result, '\0'); // get the whole string from the stream
+		cout << "Message received:" << result << endl;
 		responseStream.clear();
 
 	}
-	cout << "ReceiveString completed" << endl;
+	cout << "ReceiveMessage completed" << endl;
 }
 
-std::vector<string> connectionStrings;
+std::vector<string> connectionStrings; // holds server connection strings
 void StartProtocolClient()
 {
+	Common::DebugMessage("DEBUG StartProtocolClient");
+
 	if (connectionStrings.size() == 0)
 	{
-		cout << "Connection strings are emoty." << endl;
+		Common::ErrorMessage("Connection strings are empty.");
 		return;
 	}
+
+	cout << "Enter message (format:\"<LicenceID> <Message>\") to send or \"quit\" to quit: " << endl;
 
 	string address;
 	string port;
 	string connectionString;
-
-	//async(launch::async, &ReceiveString, ref(connectingSocket));
-	std::thread myThread;
+	thread receiveThread;
 
 	vector<string>::iterator connectionsIter = connectionStrings.begin();
 	while (keepRunning)
@@ -104,15 +107,13 @@ void StartProtocolClient()
 			return;
 		}
 
-
-		myThread = std::thread(ReceiveString, ref(connectingSocket));
-		myThread.detach();
+		receiveThread = thread(ReceiveMessage, ref(connectingSocket));
+		receiveThread.detach();
 
 		string myString;
-		while (true)
+		while (true) // loop until "quit"
 		{
 			myString.clear();
-			cout << "Message to send: ";
 			getline(cin, myString);
 			if (myString == "quit")
 			{
@@ -122,7 +123,7 @@ void StartProtocolClient()
 			}
 			else
 			{
-				if (!SendString(ref(connectingSocket), myString))
+				if (!SendMessage(ref(connectingSocket), myString))
 				{
 					break; // break for next connection
 				}
@@ -138,95 +139,6 @@ void StartProtocolClient()
 
 }
 
-void StartQuizClient()
-{
-	WinsockHelper myWinsockHelper;
-
-	string address{"localhost"};
-	string port{"3000"};
-	Socket connectingSocket(address, port);
-	int connectionResult{ connectingSocket.Connect() };
-	if (connectionResult == -1)
-	{
-		return;
-	}
-
-	stringstream requestStream{ "QUESTION" } ;
-	connectingSocket.Send(move(requestStream)); // send request for QUESTION
-
-	stringstream responseStream{ connectingSocket.Receive() }; // wait for respond
-	if (responseStream.rdbuf()->in_avail() > 0)
-	{
-		string question;
-		getline(responseStream, question, '\0');
-		responseStream.clear();
-
-		while (question != "FINISHED")
-		{
-			cout << question << endl;
-
-			string answer;
-			cin >> answer;
-
-			requestStream << "ANSWER ";
-			requestStream << answer;
-			connectingSocket.Send(move(requestStream)); // send ANSWER in request
-
-			responseStream = connectingSocket.Receive(); // wait for respond
-			if (responseStream.rdbuf()->in_avail() == 0)
-			{
-				break;
-			}
-
-			string result;
-			getline(responseStream, result, '\0');
-			cout << result << endl;
-
-			//requestStream << "QUIT";
-			requestStream << "QUESTION";
-			connectingSocket.Send(move(requestStream)); // send request for next QUESTION
-
-			responseStream = connectingSocket.Receive(); // wait for respond
-			getline(responseStream, question, '\0');
-			responseStream.clear();
-		}
-	}
-}
-
-void StartClient()
-{
-	WinsockHelper myWinsockHelper;
-
-	string address{"localhost"};
-	string port{"3000"};
-	Socket connectingSocket(address, port);
-	connectingSocket.Connect();
-
-	string messageSent{"Beauty is only skin deep."};
-	stringstream dataSent;
-	dataSent << messageSent;
-	connectingSocket.Send(move(dataSent));
-
-	cout << "Sent message." << endl;
-}
-
-void StartConnect()
-{
-	WinsockHelper myWinsockHelper;
-
-	string address{"localhost"};
-	string port{"3000"};
-	Socket connectingSocket(address, port);
-	connectingSocket.Connect();
-	connectingSocket.Close();
-
-	string myString;
-	cout << "Waiting...";
-	getline(cin, myString);
-
-}
-
-
 int main(int argc, char* argv[])
 {
 	// Check the number of command line arguments
@@ -240,14 +152,6 @@ int main(int argc, char* argv[])
 	connectionStrings.push_back(argv[2]);
 	StartProtocolClient();
 
-	/*
-	else
-	{
-		//StartConnect();
-		//StartClient();
-		StartProtocolClient();
-		//StartQuizClient();
-	}
-	*/
+	cout << endl << "Tschuess!" << endl;
 	return 0;
 }
