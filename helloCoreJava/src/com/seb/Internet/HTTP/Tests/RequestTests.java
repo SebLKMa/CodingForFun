c4a7e03999ee4d3911358c04f84711b7569188bc
seblkma <sebmalikkeung@gmail.com>
2017-06-01 13:55:02 +0800

    Added main() to run test in case JUnit cannot report http failure. UTL constructor expects complete URI string.

--- helloCoreJava/src/com/seb/Internet/HTTP/Tests/RequestTests.java
+++ helloCoreJava/src/com/seb/Internet/HTTP/Tests/RequestTests.java
@@ -14,11 +14,13 @@ import org.apache.commons.io.IOUtils;
 public class RequestTests {
 
 	@Test
-	public void makeBareHttpRequest() throws IOException {
-		final URL url = new URL("http", "www.google.com", "/");
+	public void makeBareHttpRmakeBareHttpRequestequest() throws IOException {
+		//final URL url = new URL("https", "www.google.com.sg", "/");
+		//final URL url = new URL("http", "localhost:80", "/");
+		final URL url = new URL("http://localhost:80/");
 		final HttpURLConnection connection = (HttpURLConnection)url.openConnection();
 		connection.setRequestMethod("GET");
-		
+
 		final InputStream responseInputStream = connection.getInputStream();
 		final int responseCode = connection.getResponseCode();
 		final String response = IOUtils.toString(responseInputStream);
@@ -29,4 +31,13 @@ public class RequestTests {
 		System.out.printf("Response received: [%s]%n", response);		
 	}
 	
+	public static void main(String[] args) {
+		RequestTests myTests = new RequestTests();
+		try {
+			myTests.makeBareHttpRmakeBareHttpRequestequest();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
