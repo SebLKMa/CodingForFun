61ac034ccb6829f185f5b19354acacd328f26ded
seblkma <sebmalikkeung@gmail.com>
2017-06-02 09:58:01 +0800

    added move for unique_ptr

--- helloCppMinGW/tests/ClassA.h
+++ helloCppMinGW/tests/ClassA.h
@@ -21,17 +21,29 @@ public:
 	static void test1()
 	{
 		unique_ptr<A> pA(new A);
-	}
-
-	static void test3(shared_ptr<A> p)
-	{
-		p->hello();
+		// pA will be deleted when out of scope
 	}
 
 	static void test2()
 	{
+		unique_ptr<A> pA(new A);
+		test2a(std::move(pA)); // must give release and give ownership to another unique_ptr
+	}
+
+	static void test2a(unique_ptr<A> p)
+	{
+		p->hello();
+	}
+
+	static void test3()
+	{
 		shared_ptr<A> pA(new A);
-		test3(pA);
+		test3a(pA); // another shared_ptr can point to the same object
+	}
+
+	static void test3a(shared_ptr<A> p)
+	{
+		p->hello();
 	}
 
 	static void doSimpleTest()
@@ -40,6 +52,7 @@ public:
 
 		A::test1();
 		A::test2();
+		A::test3();
 	}
 };
 
