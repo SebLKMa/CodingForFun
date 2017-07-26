b08e49c8193c0f594585a6dd106bf2a9b8f47e7d
likke <likke@LAPTOP-S2471R51>
2017-07-26 11:47:09 +0800

    added reverseSmart

--- helloSeb/src/com/seb/research/algorithms/ReverseString.java
+++ helloSeb/src/com/seb/research/algorithms/ReverseString.java
@@ -4,16 +4,42 @@ public class ReverseString {
 
     public static void main(String[] args) {
         String result = "";
-        String input= "hello world";
+        String input= "dlrow olleh";
         if (args.length == 1) {
             input = args[0];
         }
         
-        for (int i=input.length()-1; i>=0; i--) {
-            result = result + input.charAt(i);
-        }
+        //result = reverse(input);
+        result = reverseSmart(input);
         
         System.out.println(result);
     }
 
+    public static String reverse(String input) {
+        String result = "";
+        
+        for (int i=input.length()-1; i>=0; i--) {
+            result = result + input.charAt(i);
+        }
+        return result;
+    }
+    
+    public static String reverseSmart(String input) {
+        String result = "";
+        if (input.isEmpty()) return result;
+        
+        char[] inputArray = input.toCharArray();
+        
+        int len = inputArray.length;
+        int mid = (len)/2;
+        char temp;
+        for (int i=0; i<mid; i++) {
+        	// swap inputArray[i] with inputArray[len-i-1]
+    		temp = inputArray[i];
+    		inputArray[i] = inputArray[len-i-1];
+    		inputArray[len-i-1] = temp;
+        }
+        result = String.valueOf(inputArray);
+        return result;
+    }
 }
